56c823bce845d5289439bd66bdd77e4021e74ff7
구서영 <nobal@guseoyeong-ui-Macmini.local>
2025-09-15 13:52:28 +0900

    미정꺼 불러오기 전

--- src/main/frontend/src/pages/LMS/form/CreatePost.jsx
+++ src/main/frontend/src/pages/LMS/form/CreatePost.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Dropdown from '../../../components/ui/Dropdown'
 import layoutStyles from "../../../styles/layout.module.css"
-
+import { FormUnderline, FormInput, FormBtn, DeleteBtn, OptionSelect, TextAreaBox } from '../../../components/ui/UiComp';
+import styles from "../../../styles/UiComp.module.css"
 
 function CreatePost() {
   const [value, setValue] = useState("");
@@ -19,12 +20,15 @@ function CreatePost() {
         <h4 style={{fontWeight: "400"}}>게시글 등록하기</h4>
         <form className='formAreaRow' onSubmit={handleSubmit}>
           <div className='formArea_L'>
-            <div className='inputSet'></div>
+            <div className='inputSet'>
+              <FormBtn textType={"제목"}></FormBtn>
+              <FormInput textType={"제목을 입력하세요"}></FormInput>
+            </div>
           </div>
           <div className='formArea_R'>
             <div className='dropSet'>
               <p>유형</p>
-              <Dropdown label="---- 필수 선택 ----">
+              <Dropdown className="dropset_dd" label="---- 필수 선택 ----">
                 <p className={layoutStyles.subMenuList}>공지사항</p>
                 <p className={layoutStyles.subMenuList}>자료실</p>
                 <p className={layoutStyles.subMenuList}>학습일지</p>
@@ -32,6 +36,7 @@ function CreatePost() {
                 <p className={layoutStyles.subMenuList}>Q&A</p>
               </Dropdown>
             </div>
+            <OptionSelect></OptionSelect>
           </div>
         </form>
       </div>
