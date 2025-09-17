d425b6b569ccdf6065d6c9e3b2b0ef19e024658b
KimsangJ <144760434+KimsangJ@users.noreply.github.com>
2025-09-17 11:13:24 +0900

    과정 찾기에 회사sn추가

--- src/main/java/com/kdt/KDT_PJT/cohort/repository/CohortRepository.java
+++ src/main/java/com/kdt/KDT_PJT/cohort/repository/CohortRepository.java
@@ -4,6 +4,11 @@ package com.kdt.KDT_PJT.cohort.repository;
 import com.kdt.KDT_PJT.cohort.entity.Cohort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CohortRepository extends JpaRepository<Cohort, Long> {
+
+    List<Cohort> findByCoSn(Long coSn);
     // 추가 쿼리가 필요하면 작성
 }
