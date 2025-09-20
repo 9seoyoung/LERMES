3c4e78f6ad3897c2fa70b676618e4c31387eabd3
구서영 <nobal@guseoyeong-ui-Macmini.local>
2025-09-20 13:27:46 +0900

    다시 deployURL로 전환

--- src/main/frontend/src/auth/api.js
+++ src/main/frontend/src/auth/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 // 배포용
 
-const deployURL = 'onopco2.iptime.org:8080'
+const deployURL = 'onopco2.iptime.org:940'
 // const developURL = 'localhost:940'
 
 export const api = axios.create({
-  baseURL: `/api`,
+  baseURL: `http://${deployURL}/api`,
   withCredentials: true, // 세션쿠키 자동 전송
   xsrfCookieName: 'XSRF-TOKEN', // 시큐리티 쿠키 기본 토큰
   xsrfHeaderName: 'X-XSRF-TOKEN', //시큐리티
