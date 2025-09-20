491b82e188c557efa2682bb3f845756018e7f175
구서영 <nobal@guseoyeong-ui-Macmini.local>
2025-09-20 17:00:03 +0900

    api.js baseURL에서 도메인 제거

--- src/main/frontend/src/auth/api.js
+++ src/main/frontend/src/auth/api.js
@@ -6,11 +6,11 @@ import axios from 'axios';
 // 아이피타임 포트포워드인가 그것도 맞춰줘야하는듯? 그것도 맞췄음.
 // 배치파일도 포트번호 맞춰줘야되는듯? 일단 맞췃음.
 // 배포는 https로 443
-const deployURL = 'http://onopco2.iptime.org:940'
+//const deployURL = 'http://onopco2.iptime.org:940' <<--- proxy설정해서 필요없음
 // const developURL = 'http://localhost:940'
 
 export const api = axios.create({
-  baseURL: `${deployURL}/api`,
+  baseURL: `/api`,
   withCredentials: true, // 세션쿠키 자동 전송
   xsrfCookieName: 'XSRF-TOKEN', // 시큐리티 쿠키 기본 토큰
   xsrfHeaderName: 'X-XSRF-TOKEN', //시큐리티
