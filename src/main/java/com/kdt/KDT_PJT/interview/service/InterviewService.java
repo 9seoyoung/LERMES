52190b8a4ea8f2540a554938c24e567380486651
Dongsik-00 <ehdtlr0012@gmail.com>
2025-09-21 00:42:16 +0900

    면담신청 API 구현중, 파일등록 api 수정(폼UUID 등록 가능하게)

--- src/main/java/com/kdt/KDT_PJT/interview/service/InterviewService.java
+++ src/main/java/com/kdt/KDT_PJT/interview/service/InterviewService.java
@@ -6,9 +6,12 @@ import com.kdt.KDT_PJT.cmmn.map.CmmnMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,21 +24,26 @@ public class InterviewService {
      * @methodName : createInterviewRequest
      * @author : 김동식
      * @date : 2025.09.19
-     * @param me : 로그인한 사용자 정보
+     * @param me : 로그인한 사용자 정보(교사 또는 학생)
      * @param params : 면담 정보(담당자, 대상자, 일시 등)를 담은 Map
      * @return : 성공 여부 및 저장된 정보를 담은 Map
      * Description : 면담 이벤트와 기록을 생성하고 데이터베이스에 저장합니다.
      * 하나의 면담에 하나의 기록만 존재하는 1:1 관계를 가집니다.
      */
+    @PreAuthorize("hasAnyRole('INSTRUCTOR','STUDENT')") //얘네만 면담신청 가능
     @Transactional
     public CmmnMap createInterviewRequest(AuthCustomUserDetails me, CmmnMap params) {
         // 0) 인증 확인
         if (me == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 필요");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 필요"); //예외 발생시 정상응답(200ok)대신 상태코드에 맞는 HTTP응답을 클라이언트에 돌려보냄
         }
+        //params에 담긴 정보 : 면담 신청 제목, 면담 신청 내용, 면담 담당자 권한
+        params.put("itvAplcntSn", me.getId());   //로그인유저 사용자SN 가져옴
+        params.put("cohortSn", me.getCohortId());  //로그인 유저의 기수SN 가져옴
+        String uuid = UUID.randomUUID().toString().replace("-", ""); //하이픈 제거된 uuid 얻음(신청글에대한 uuid)
+        params.put("formUuid", uuid);              //신청글에대한 uuid 만듦
 
-        // 1) 기본 유효성
-
+        dao.insert("com.kdt.mapper.interview.insertApplyInterview", params);
 
         CmmnMap result = new CmmnMap();
         return result;
