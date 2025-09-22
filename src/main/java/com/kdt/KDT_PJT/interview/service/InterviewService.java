db74feae4a4bb8bf0a41057ac07f7bbcad343aca
Dongsik-00 <ehdtlr0012@gmail.com>
2025-09-22 12:24:48 +0900

    사용자SN, 기수SN 안들어가는 오류 수정중 (면담)

--- src/main/java/com/kdt/KDT_PJT/interview/service/InterviewService.java
+++ src/main/java/com/kdt/KDT_PJT/interview/service/InterviewService.java
@@ -41,14 +41,35 @@ public class InterviewService {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 필요");   //예외 발생시 정상응답(200ok)대신 상태코드에 맞는 HTTP응답을 클라이언트에 돌려보냄
         }
         //params에 담긴 정보 : 면담 신청 제목, 면담 신청 내용, 면담 담당자 권한
-        params.put("itvAplcntSn", me.getId());      //로그인유저 사용자SN 가져옴
-        params.put("cohortSn", me.getCohortSn());   //로그인 유저의 기수SN 가져옴
+        params.put("itvAplcntSn", Math.toIntExact(me.getId()));      //로그인유저 사용자SN 가져옴
+        params.put("cohortSn", Math.toIntExact(me.getCohortSn()));   //로그인 유저의 기수SN 가져옴
         String uuid = UUID.randomUUID().toString().replace("-", ""); //하이픈 제거된 uuid 얻음(신청글에대한 uuid) // TODO 일단 모든 면담에 대해 uuid 만드는데, 파일 있을경우만 생성하도록 변경할팔요
         params.put("formUuid", uuid);               //신청글에대한 uuid 만듦
         params.put("itvAplyDt", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)); // 면담 신청 일시 추가(기록용)
 
+        //itvPicAuthrt 값을 문자열로 가져와서 Integer로 변환
+        String authrtString = (String) params.get("itvPicAuthrt");
+        Integer authrtCode = null;
+        if ("대표".equals(authrtString)) {
+            authrtCode = 2;
+        } else if ("직원".equals(authrtString)) {
+            authrtCode = 3;
+        } else if ("강사".equals(authrtString)) {
+            authrtCode = 4;
+        } else {
+            // 변환할 수 없는 값일 경우 예외 처리
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 면담 담당자 권한입니다.");
+        }
+        System.out.println("면담대상자 authrtCode = " + authrtCode);
+        // 2) 변환된 Integer 값을 다시 CmmnMap에 넣기
+        params.put("itvPicAuthrt", authrtCode);
+
+
         dao.insert("com.kdt.mapper.interview.insertApplyInterview", params); //itvSn 생성되어 들어옴.
         System.out.println("params = " + params);
+        System.out.println(params.get("itvPicAuthrt"));
+        System.out.println(params.get("itvPicAuthrt").getClass().getName());
+        System.out.println(params.get("itvAplcntSn").getClass().getName());
 //        CmmnMap result = new CmmnMap();
         CmmnMap result = params;
         return result;
