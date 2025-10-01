b2e07c024712083d1d5194b496766417a24da799
jinryxxng <glasofk8997@naver.com>
2025-10-01 17:11:05 +0900

    ãbefore chedckout 20251001

--- src/main/java/com/kdt/KDT_PJT/survey/ctl/SurveyController.java
+++ src/main/java/com/kdt/KDT_PJT/survey/ctl/SurveyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bbsSurvey")
+@RequestMapping("api/survey")
 @RequiredArgsConstructor
 public class SurveyController {
 
