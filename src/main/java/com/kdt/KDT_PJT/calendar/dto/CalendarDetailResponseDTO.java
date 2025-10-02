a99cb89fae4bebfd3b2451fbce9692520645829d
Dongsik-00 <ehdtlr0012@gmail.com>
2025-10-02 14:25:40 +0900

    프론트용 시간 파싱 추가

--- src/main/java/com/kdt/KDT_PJT/calendar/dto/CalendarDetailResponseDTO.java
+++ src/main/java/com/kdt/KDT_PJT/calendar/dto/CalendarDetailResponseDTO.java
@@ -1,6 +1,7 @@
 package com.kdt.KDT_PJT.calendar.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +16,36 @@ import java.time.LocalDateTime;
 @AllArgsConstructor         // Data + builder 조합일때는 @AllArgsConstructor가 자동 생성되서 상관 없는데 @NoArgsConstructor명시하면 자동생성 더이상 안해서 써줘야함
 public class CalendarDetailResponseDTO {
     private Integer calSn;
-    private LocalDateTime eventBgngDt;  //EVENT_BGNG_DT
-    private LocalDateTime eventEndDt;   //EVENT_END_DT
+    @JsonIgnore private LocalDateTime eventBgngDt;  //EVENT_BGNG_DT
+    @JsonIgnore private LocalDateTime eventEndDt;   //EVENT_END_DT
+    @JsonProperty("title")
     private String eventNm;             //EVENT_NM 이벤트 이름
+    @JsonProperty("memo")
     private String rmrkCn;              //RMRK_CN 이벤트 설명
-    private String userNm;                // 작성자명, TB_USER에서 userSn으로 조인해서 가져오기
+    @JsonProperty("location")
+    private String eventPlc;            //이벤트 장소
+    private String userNm;              // 작성자명, TB_USER에서 userSn으로 조인해서 가져오기
     private LocalDateTime eventRegDt;   //EVENT_REG_DT 작성일
     private Byte prvtYn;                //PRVT_YN 개인일정이면1 아니면 0
-    //    private Integer coSn;             //굳이? 회사정보 보내줄필요는없을듯
+    //    private Integer coSn;         //굳이? 회사정보 보내줄필요는없을듯
     private Integer viewCnt;            // 조회수 (상세보기 눌렀을떄 up)
     @JsonIgnore private Integer userSn;             //USER_SN 사용자 일련번호 이거는 보내줄필요없을듯,
     @JsonIgnore private Integer cohortSn;           //COHORT_SN
+
+    //Json으로 내보낼때 변환
+    public String getStartDate() {
+        return eventBgngDt != null ? eventBgngDt.toLocalDate().toString() : null;
+    }
+
+    public String getStartTime() {
+        return eventBgngDt != null ? eventBgngDt.toLocalTime().toString() : null;
+    }
+
+    public String getEndDate() {
+        return eventEndDt != null ? eventEndDt.toLocalDate().toString() : null;
+    }
+
+    public String getEndTime() {
+        return eventEndDt != null ? eventEndDt.toLocalTime().toString() : null;
+    }
 }
