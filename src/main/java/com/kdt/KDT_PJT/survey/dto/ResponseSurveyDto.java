e6ddf401b0aca0fbd29c595f02ad8e676eebb960
940 <wutan1998@gmail.com>
2025-10-02 11:32:54 +0900

    진령머지

--- src/main/java/com/kdt/KDT_PJT/survey/dto/ResponseSurveyDto.java
+++ src/main/java/com/kdt/KDT_PJT/survey/dto/ResponseSurveyDto.java
@@ -1,6 +1,6 @@
 package com.kdt.KDT_PJT.survey.dto;
 //설문 등록/조회/수정
-import com.kdt.KDT_PJT.survey.enums.SurveyType;
+//import com.kdt.KDT_PJT.survey.enums.SurveyType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +21,5 @@ public class ResponseSurveyDto {
     private LocalDate srvyEndDt;    // 설문 종료일
     private String userNm;          // 작성자
 
-    private SurveyType type;        // 응답 시 "설문"
+//    private SurveyType type;        // 응답 시 "설문"
 }
