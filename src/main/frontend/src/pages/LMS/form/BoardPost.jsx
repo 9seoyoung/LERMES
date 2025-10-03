c5f895355bf1618f3f9bbbba79974d4c8111c0a8
Seoyoung Koo <wutan1998@gmail.com>
2025-10-03 22:27:33 +0900

    관리자 게시글 등록 연결

--- src/main/frontend/src/pages/LMS/form/BoardPost.jsx
+++ src/main/frontend/src/pages/LMS/form/BoardPost.jsx
@@ -10,7 +10,7 @@ import {v4 as uuidv4} from "uuid";
 import { createSurvey, createPost } from '../../../services/postService';
 import { uploadFiles } from '../../../services/fileService';
 import { toast } from 'react-toastify';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelectedCompany } from '../../../contexts/SelectedCompanyContext';
 
 
@@ -79,6 +79,7 @@ function BoardPost() {
   const scrollRef = useRef(null);
   const locate = useLocation();
   const {effectiveSn} = useSelectedCompany();
+  const navigate = useNavigate();
   // const [loading, setLoading] = useState(false);
 
   const [hortlist, setHortList] = useState([]);
@@ -129,19 +130,19 @@ function BoardPost() {
       })
     : [];
 
-  // 2) 게시글 JSON (File 객체 넣지 말기!)
-  const postJson = {
-    id: formData.id,
-    userSn: formData.userSn,
-    title: formData.title,
-    content: formData.content,
-    coSn: formData.coSn,
-    type: formData.type,
-    cohortSn: cohortSn,
-    scope: formData.scope,
-    detailScope: formData.detailScope,
-    detailScopeNm: formData.detailScopeNm,
-    attachments: uploads.map(u => ({
+    // 2) 게시글 JSON (File 객체 넣지 말기!)
+    const postJson = {
+      id: formData.id,
+      userSn: formData.userSn,
+      title: formData.title,
+      content: formData.content,
+      coSn: formData.coSn,
+      type: formData.type,
+      cohortSn: cohortSn,
+      scope: formData.scope,
+      detailScope: formData.detailScope,
+      detailScopeNm: formData.detailScopeNm,
+      attachments: uploads.map(u => ({
       storedFileName: u.storedFileName,
       originalFileName: u.originalFileName,
       size: u.size,
@@ -160,7 +161,6 @@ function BoardPost() {
   console.log("[will send to server]", JSON.stringify(body, null, 2));
   console.table(snapshot.formData);
 
-  console.time("[RecruitPost] createGroup");
   // 3) 게시글 저장 (설문이면 createSurvey, 일반이면 createPost)
   try {
     const res = await (formData.type === "설문조사"
@@ -168,6 +168,7 @@ function BoardPost() {
       : createPost(postJson)); // createPost는 너희 규약대로
     console.log("saved:", res);
     alert("저장 완료!");
+    navigate(-1);
   } catch (err) {
     console.error(err);
     toast.error(err.message);
@@ -257,10 +258,8 @@ function BoardPost() {
                     <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "그룹공개" }))}>그룹공개</p>
                     <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "비공개" }))}>비공개</p>
                   </Dropdown>
-                    <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "관리자" }))}>관리자</p>
-                    <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "강사" }))}>강사</p>
                   </>: null }
-                  { ( userAuth === 4 || userAuth === 5) && (effectiveSn === coSn) ? <>
+                  { ( userAuth === 4 || userAuth === 5) && (effectiveSn === coSn) ?<>
                     <Dropdown className="dropset_dd" label={formData.scope || "---- 필수 선택 ----"}>
                       <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "그룹공개", cohortSn: cohortSn }))}>그룹공개</p>
                       <p className={layoutStyles.subMenuList} onClick={() => setFormData(s => ({ ...s, scope: "비공개" }))}>비공개</p>
@@ -270,7 +269,7 @@ function BoardPost() {
                 <input type="hidden" name="scope" value={formData.scope} />
               </div>
 
-              {formData.scope === "소속그룹" && (
+              {formData.scope === "그룹공개" && (
                 <div className="dropSet" style={{ zIndex: "1" }}>
                   <p>하위 그룹</p>
                   <Dropdown className="dropset_dd" label={formData.detailScopeNm || "---- 필수 선택 ----"}>
@@ -279,7 +278,7 @@ function BoardPost() {
                         className={layoutStyles.subMenuList}
                         key={idx}
                         onClick={() => {
-                          setFormData(s => ({ ...s, detailScope: h.cohortSn, detailScopeNm: String(h.cohortNm) }));
+                          setFormData(s => ({ ...s, cohortSn:h.cohortSn, detailScope: h.cohortSn, detailScopeNm: String(h.cohortNm) }));
 
                         }}
                       >
