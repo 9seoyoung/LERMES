228565b88810d582d017fa1ad978253b283d014d
KimsangJ <144760434+KimsangJ@users.noreply.github.com>
2025-10-13 22:54:22 +0900

    여러가지

--- src/main/java/com/kdt/KDT_PJT/companymem/ctl/CompanyMemberController.java
+++ src/main/java/com/kdt/KDT_PJT/companymem/ctl/CompanyMemberController.java
@@ -63,16 +63,16 @@ public class CompanyMemberController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/{memberId}/approve")
-    public ResponseEntity<String> approveCompanyMember(@PathVariable Long memberId) {
-        companyMemberService.approveMember(memberId);
-        return ResponseEntity.ok("승인 완료");
+    @PostMapping("/{companyMemberSn}/approve")
+    public ResponseEntity<String> approveCompanyMember(@PathVariable Long companyMemberSn) {
+        companyMemberService.approveMember(companyMemberSn);
+        return ResponseEntity.ok("승인 완료 (ID: " + companyMemberSn + ")");
     }
 
-    @PostMapping("/{memberId}/reject")
-    public ResponseEntity<String> rejectCompanyMember(@PathVariable Long memberId) {
-        companyMemberService.rejectMember(memberId);
-        return ResponseEntity.ok("반려 완료");
+    @PostMapping("/{companyMemberSn}/reject")
+    public ResponseEntity<String> rejectCompanyMember(@PathVariable Long companyMemberSn) {
+        companyMemberService.rejectMember(companyMemberSn);
+        return ResponseEntity.ok("반려 완료 (ID: " + companyMemberSn + ")");
     }
 
 
