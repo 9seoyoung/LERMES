b184fdd9d098bec4fe6f57e70f07801775eaa6e8
chan <klp100119@gmail.com>
2025-10-14 13:38:01 +0900

    logoutSuccessUrl 주소변환

--- src/main/java/com/kdt/KDT_PJT/auth/config/AuthSecurityConfig.java
+++ src/main/java/com/kdt/KDT_PJT/auth/config/AuthSecurityConfig.java
@@ -101,7 +101,8 @@ public class AuthSecurityConfig {
                         .logoutUrl("/logout")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
-                        .logoutSuccessUrl("http://localhost:3000/welcome/login")
+                        //.logoutSuccessUrl("http://localhost:3000/welcome/login")
+                        .logoutSuccessUrl("http://onopco2.iptime.org:3000/welcome/login")
                 )
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
