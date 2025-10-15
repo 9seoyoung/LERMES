597cbccd9cde4c1a2717e3eede71774753992ec9
jinryxxng <glasofk8997@naver.com>
2025-10-15 21:40:51 +0900

    트리구조화된 대댓글

--- src/main/java/com/kdt/KDT_PJT/comment/mapper/CommentMapper.java
+++ src/main/java/com/kdt/KDT_PJT/comment/mapper/CommentMapper.java
@@ -9,22 +9,6 @@ import java.util.List;
 @Mapper
 public interface CommentMapper {
 
-//    // 댓글 등록
-//    void insertComment(@Param("dto") CommentDto dto);
-//
-//    // 게시글 기준 댓글 전체 조회
-//    List<CommentDto> findCommentsByPost(@Param("postSn") Long postSn);
-//
-//    // 댓글 단건 조회
-//    CommentDto findCommentById(@Param("cmntSn") Long cmntSn);
-//
-//    // 댓글 수정
-//    void updateComment(@Param("dto") CommentDto dto);
-//
-//    // 댓글 삭제 (Soft Delete)
-//    void softDeleteComment(@Param("cmntSn") Long cmntSn,
-//                           @Param("userSn") Long userSn);
-//    List<CommentDto> findRepliesByParent(@Param("parentCmntSn") Long parentCmntSn);
 
     void insertComment(CommentDto dto);
 
